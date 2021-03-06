51c74f0a843ae0878272fc1b2ca87797b4f98648
Jorge Naranjo <jorgearturodecher@gmail.com>
2021-03-06 10:17:00 -0300

    add customizable theme

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Container, Content, Text, StyleProvider } from 'native-base';
+import getTheme from './native-base-theme/components';
+import material from './native-base-theme/variables/material';
+import * as Font from 'expo-font';
 export default function App() {
+
+  const [isReady, setReady] = React.useState(false)
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+    })
+    setReady(true)
+  }
+
+  React.useEffect( ()=> {
+    loadFonts()
+  }, [isReady])
+
+  if(!isReady) {
+    return <Text>Loading</Text>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   <StyleProvider style={getTheme(material)}>
+    <Container>
+      <Content>
+        <Text>
+          I have changed the text color.
+        </Text>
+      </Content>
+    </Container>
+  </StyleProvider>
+ );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
